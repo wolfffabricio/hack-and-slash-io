1c4590696b7a95734ea8e52fa28a587cf25416be
Arthur Oliveira <abol2109@gmail.com>
2020-05-23 12:05:22 -0300

    Criação Projeto Unity

---
+++ HackAndSlash/Assets/Script/Lance.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lance : ItensParent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        onGround = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void PickItem()
+    {
+        if (onGround)
+        {
+            onGround = false;
+            Debug.Log("Peguei uma lança");
+        }
+    }
+
+    public override void UseItem()
+    {
+        Debug.Log("Ataquei com lança");
+    }
+}
