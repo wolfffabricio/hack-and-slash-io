ea5a94087a12f56a7113a99a2d489e97d1c32ffc
Arthur Oliveira <abol2109@gmail.com>
2020-05-25 19:10:07 -0300

    Refatoração do código

--- HackAndSlash/Assets/Script/ItensParent.cs
+++ HackAndSlash/Assets/Script/ItensParent.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public abstract class ItensParent : MonoBehaviour
 {
     protected bool onGround;
+    protected bool attacking;
+    protected int charges;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        onGround = true;
+        attacking = false;
+    }
 
     public virtual void PickItem()
     {
         onGround = false;
+        Debug.Log("Peguei um Item Generico");
     }
 
    public virtual void UseItem()
     {
-        
+        Debug.Log("Usei um Item Generico");
     }
 }
