e800ac840ff4c0d6f99a6a6ed367c7f2f41b0290
Arthur Oliveira <abol2109@gmail.com>
2020-05-25 19:10:07 -0300

    Refatoração do código

--- HackAndSlash/Assets/Script/Lance.cs
+++ HackAndSlash/Assets/Script/Lance.cs
@@ -7,7 +7,7 @@ public class Lance : ItensParent
     // Start is called before the first frame update
     void Start()
     {
-        onGround = true;
+        charges = 10;
     }
 
     // Update is called once per frame
@@ -21,12 +21,29 @@ public class Lance : ItensParent
         if (onGround)
         {
             onGround = false;
+            
+            Vector3 newPosition = new Vector3(0, 0, 1);
+            transform.localPosition = newPosition;
+            
             Debug.Log("Peguei uma lança");
         }
     }
 
     public override void UseItem()
     {
+        charges--;
+
         Debug.Log("Ataquei com lança");
+        attacking = true;
+
+        //TODO wait
+
+        attacking = false;
+
+        if(charges==0)
+        {
+            //TODO destruir item
+
+        }
     }
 }
