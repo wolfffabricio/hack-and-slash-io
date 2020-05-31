0e575c719e0e80e3c90cd0389d8aaeefb0a6a413
Fabricio <wolfffabricio@gmail.com>
2020-05-30 21:03:30 -0300

    Invetory and equipment classes

---
+++ HackAndSlash/Assets/Script/Invetory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invetory : MonoBehaviour
+{
+    private List<Jewellery> myJewellery = new List<Jewellery>();
+    private int lootboxAvailable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Jewellery j = new Jewellery("Anel de Força", "Anel", 5, 0);
+        myJewellery.Add(j);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
