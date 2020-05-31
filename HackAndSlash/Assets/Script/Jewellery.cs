a5045627afd6318e079a0b8f9dbfd906ffc3bb0b
Fabricio <wolfffabricio@gmail.com>
2020-05-30 21:03:30 -0300

    Invetory and equipment classes

---
+++ HackAndSlash/Assets/Script/Jewellery.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jewellery : MonoBehaviour
+{
+    private bool isEquiped;
+    private string name;
+    private string type;
+    private int powerBonus;
+    private int healthBonus;
+
+    public Jewellery(string name, string type, int powerBonus, int healthBonus)
+    {
+        this.name = name;
+        this.type = type;
+        this.powerBonus = powerBonus;
+        this.healthBonus = healthBonus;
+    }
+
+    public bool IsEquiped { get => isEquiped; set => isEquiped = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
