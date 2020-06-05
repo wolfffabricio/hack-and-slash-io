ffb69d83834cdd1eaafb163c121127a5c4c6be4a
Arthur Oliveira <abol2109@gmail.com>
2020-06-05 16:20:49 -0300

    Inventory Improvments
    now the accessories have images

--- HackAndSlash/Assets/Script/Jewellery.cs
+++ HackAndSlash/Assets/Script/Jewellery.cs
@@ -9,14 +9,16 @@ public class Jewellery : MonoBehaviour
     private int powerBonus;
     private int healthBonus;
     private bool isEquiped;
+    private int level;
 
-    public Jewellery(string jewellName, string jewellType, int powerBonus, int healthBonus,bool isEquiped)
+    public Jewellery(string jewellName, string jewellType, int powerBonus, int healthBonus,bool isEquiped,int level)
     {
         this.jewellName = jewellName;
         this.jewellType = jewellType;
         this.powerBonus = powerBonus;
         this.healthBonus = healthBonus;
         this.isEquiped = isEquiped;
+        this.level = level;
     }
 
     public string JewellName { get => jewellName; set => jewellName = value; }
@@ -24,6 +26,7 @@ public class Jewellery : MonoBehaviour
     public int PowerBonus { get => powerBonus; set => powerBonus = value; }
     public int HealthBonus { get => healthBonus; set => healthBonus = value; }
     public bool IsEquiped { get => isEquiped; set => isEquiped = value; }
+    public int Level { get => level; set => level = value; }
 
     // Start is called before the first frame update
     void Start()
