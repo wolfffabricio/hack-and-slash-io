8fffe2d1cf91ac74372b328a3aee61c65e0eaca7
Arthur Oliveira <abol2109@gmail.com>
2020-06-11 18:36:42 -0300

    Inventory Update

--- HackAndSlash/Assets/Script/UI/InventoryUI.cs
+++ HackAndSlash/Assets/Script/UI/InventoryUI.cs
@@ -6,11 +6,18 @@ using UnityEngine.UI;
 public class InventoryUI : MonoBehaviour
 {
     Button equip, unequip;
-    Text[] equppedItens = new Text[5];
+    Text[] equippedItemNames = new Text[5];
+    Text[] equippedItemPower = new Text[5];
+    Text[] equippedItemHealth = new Text[5];
     GameObject gridLayoutContent;
     GameObject prefabImg;
 
-    //User user;
+    Text selectedJewellName;
+    Text selectedJewellPower;
+    Text selectedJewellHealth;
+    Text moneyText;
+
+    User user;
     Inventory inventory;
 
     Jewellery selectedJewellery = null;
@@ -25,10 +32,17 @@ public class InventoryUI : MonoBehaviour
 
         for (int i = 0; i < 5; i++)
         {
-            equppedItens[i] = GameObject.Find("EquippedItem" + i).GetComponent<Text>();
+            equippedItemNames[i] = GameObject.Find("EquippedItem" + i).GetComponent<Text>();
+            equippedItemPower[i] = GameObject.Find("EquippedItemPower" + i).GetComponent<Text>();
+            equippedItemHealth[i] = GameObject.Find("EquippedItemHealth" + i).GetComponent<Text>();
         }
 
-        //user = GameObject.Find("User").GetComponent<User>();
+        selectedJewellName = GameObject.Find("TextSelectedJewellName").GetComponent<Text>();
+        selectedJewellPower = GameObject.Find("TextSelectedJewellPower").GetComponent<Text>();
+        selectedJewellHealth = GameObject.Find("TextSelectedJewellHealth").GetComponent<Text>();
+        moneyText = GameObject.Find("TextMoney").GetComponent<Text>();
+
+        user = GameObject.Find("User").GetComponent<User>();
         inventory = GameObject.Find("Inventory").GetComponent<Inventory>();
 
 
@@ -39,14 +53,17 @@ public class InventoryUI : MonoBehaviour
         gridLayoutContent.GetComponent<GridLayoutGroup>().cellSize = v;
     }
 
-    public void PopulateGrid(List<Jewellery> myJewellery)
+    /*public*/
+    void PopulateGrid(List<Jewellery> myJewellery)
     {
+        RestartGrid();
+
         int[,] grid = new int[5, Mathf.CeilToInt(myJewellery.Count / 5.0f)];
         GameObject newobj;
 
         for (int i = 0; i < myJewellery.Count; i++)
         {
-            newobj = (GameObject)Instantiate(prefabImg, gridLayoutContent.transform);
+            newobj = /*(GameObject)*/Instantiate(prefabImg, gridLayoutContent.transform);
             Sprite s = Resources.Load<Sprite>("Jewellery_Images/" + myJewellery[i].JewellType + "_" + myJewellery[i].Level);
             newobj.GetComponent<Image>().sprite = s;
 
@@ -59,27 +76,59 @@ public class InventoryUI : MonoBehaviour
         }
     }
 
+    void RestartGrid()
+    {
+        for (int i = 0; i < gridLayoutContent.transform.childCount; i++)
+        {
+            Destroy(gridLayoutContent.transform.GetChild(i).GetComponent<Image>());
+            Destroy(gridLayoutContent.transform.GetChild(i).gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        UpdateTexts();
+        PopulateGrid(inventory.MyJewellery);
+
+        UpdateEquippedJewellTexts();
+        UpdateMoneyText();
     }
 
-    void UpdateTexts()
+    void UpdateEquippedJewellTexts()
     {
-        string[] equippedJewellsName = inventory.EquippedJewellsName();
+        string[] equippedJewellsName = new string[inventory.ActiveJewellery.Count];
+        string[] equippedJewellsPower = new string[inventory.ActiveJewellery.Count];
+        string[] equippedJewellsHealth = new string[inventory.ActiveJewellery.Count];
 
-        for (int i = 0; i < equppedItens.Length; i++)
+        int count = 0;
+        foreach (KeyValuePair<string, Jewellery> activeJ in inventory.ActiveJewellery)
+        {
+            equippedJewellsName[count] = activeJ.Value.JewellName;
+            equippedJewellsPower[count] = activeJ.Value.PowerBonus.ToString();
+            equippedJewellsHealth[count] = activeJ.Value.HealthBonus.ToString();
+            count++;
+        }
+
+        for (int i = 0; i < equippedItemNames.Length; i++)
         {
             if (i + 1 > equippedJewellsName.Length)
             {
-                equppedItens[i].text = "-";
+                equippedItemNames[i].text = "-";
+                equippedItemPower[i].text = "-";
+                equippedItemHealth[i].text = "-";
             }
             else
             {
-                equppedItens[i].text = equippedJewellsName[i];
+                equippedItemNames[i].text = equippedJewellsName[i];
+                equippedItemPower[i].text = equippedJewellsPower[i];
+                equippedItemHealth[i].text = equippedJewellsHealth[i];
             }
         }
+    }
+
+    void UpdateMoneyText()
+    {
+        moneyText.text = "Dinheiro:" + user.Gold.ToString();
     }
 
     // Update is called once per frame
@@ -101,6 +150,10 @@ public class InventoryUI : MonoBehaviour
             equip.interactable = true;
             unequip.interactable = false;
         }
+
+        selectedJewellName.text = j.JewellName;
+        selectedJewellPower.text = "Poder:" + j.PowerBonus;
+        selectedJewellHealth.text = "Vida:" + j.HealthBonus;
     }
 
     public void BackButton()
@@ -113,7 +166,7 @@ public class InventoryUI : MonoBehaviour
         inventory.EquipJewell(selectedJewellery);
         ShowEquipOrUnequipButton(ref selectedJewellery);
 
-        UpdateTexts();
+        UpdateEquippedJewellTexts();
         inventory.WriteString();
     }
 
@@ -122,7 +175,17 @@ public class InventoryUI : MonoBehaviour
         inventory.UnequipJewell(selectedJewellery);
         ShowEquipOrUnequipButton(ref selectedJewellery);
 
-        UpdateTexts();
+        UpdateEquippedJewellTexts();
         inventory.WriteString();
+    }
+
+    public void BuyButton()
+    {
+        if (inventory.BuyLootBox())
+        {
+            UpdateMoneyText();
+
+            PopulateGrid(inventory.MyJewellery);
+        }
     }
 }
